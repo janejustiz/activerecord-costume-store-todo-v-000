50803092003b69967be3559356d51789b8cb9b37
Jane Justiz <janejustiz@gmail.com>
2019-08-20 15:09:11 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,6 +1,6 @@
 class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
-    create_table :costumes |t|
+    create_table :costumes do |t|
       t.string :name
       t.integer :price
       t.string :size
