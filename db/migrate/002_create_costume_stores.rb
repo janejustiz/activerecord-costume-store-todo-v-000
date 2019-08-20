ca50d66b7c62d4bea62024ddf92ccdec745d1220
Jane Justiz <janejustiz@gmail.com>
2019-08-20 15:20:31 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,8 +6,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
-      t.string :opening_time #not string
-      t.string :closing_time #not string
+      t.datetime :opening_time #not string
+      t.datetime :closing_time #not string
     end
   end
 end
