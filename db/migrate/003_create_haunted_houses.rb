d60ab110c93b5586d02bd3954aa5e8fc5ded18ed
Jane Justiz <janejustiz@gmail.com>
2019-08-20 14:50:08 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,14 @@
-# Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location #not string
+      t.string :theme
+      t.integer :price #has decimals
+      t.boolean :family_friendly
+      t.string :opening_time #date?
+      t.string :closing_time #date?
+      t.string :long_description
+    end
+  end
+end
