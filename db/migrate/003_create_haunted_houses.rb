a76ff345058d7fd59059192c720e7dde59a7e068
Jane Justiz <janejustiz@gmail.com>
2019-08-20 14:55:20 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -4,11 +4,11 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.string :name
       t.string :location #not string
       t.string :theme
-      t.integer :price #has decimals
+      t.float :price #has decimals
       t.boolean :family_friendly
       t.string :opening_time #date?
       t.string :closing_time #date?
-      t.string :long_description
+      t.text :long_description
     end
   end
 end
